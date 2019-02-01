613dea2b760f8f06d721dc77340765f2d5a57ef2
Alain P <alain.paolini291@gmail.com>
2019-02-01 09:08:30 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Ui-jar Schematics
+
+## 1 - Set up an initial ui-jar project
+
+```
+ng g ui-jar-schematics:generate-uijar-project [options]
+```
+
+- creates a project named 'ui-jar' in the projects directory of the Angular workspace
+- downloads the ui-jar dependency
+- sets up the project : adds script to package.json file, updates tsconfig.app.json, replaces main.ts and index.html files
+
+```
+options :
+
+--skipInstall=true|false (default: false)
+```
+
+This initial project works for the default project (if not skipped) created within the Angular workspace by the `ng new` command
+
+### Run the style guide
+
+```
+npm run start:ui-jar
+```
+
+## 2 - Add configuration for a specific project or library
+
+```
+ng g ui-jar-schematics:add-uijar-doc [options]
+```
+
+- creates a specific tsconfig.app.json for the project or the library
+- add build configuration is angular.json file corresponding to the new documentation to generate for the project or the library : this will specify the output directory and the tsconfig.app.json that will replace the initial one
+- adds script to package.json file
+
+```
+options :
+
+--project=name (a prompt asks it if not specified)
+```
+
+### Run the style guide for the specific project or library
+
+```
+npm run start:ui-jar:{project-name}
+```
---
+++ src/generate-uijar-project/index.ts
@@ -0,0 +1,96 @@
+import {
+  Rule,
+  SchematicContext,
+  Tree,
+  externalSchematic,
+  mergeWith,
+  apply,
+  url,
+  move,
+  chain,
+  noop
+} from '@angular-devkit/schematics'
+import {
+  addPackageJsonDependency,
+  NodeDependency,
+  NodeDependencyType
+} from '@schematics/angular/utility/dependencies'
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks'
+import { Schema as ApplicationOptions } from '@schematics/angular/application/schema'
+
+import { Schema as GenerateUiJarProjectOptions } from './schema'
+
+export default function(options: GenerateUiJarProjectOptions): Rule {
+  return chain([
+    (_tree: Tree, context: SchematicContext) => {
+      !options.skipInstall ? context.addTask(new NodePackageInstallTask()) : noop()
+    },
+    externalSchematic<ApplicationOptions>('@schematics/angular', 'application', {
+      name: 'ui-jar',
+      skipInstall: true,
+      minimal: true
+    }),
+    addUIJarDependency(),
+    setupUIJarProject()
+  ])
+}
+
+function addUIJarDependency(): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    const uijarDependency: NodeDependency = {
+      name: 'ui-jar',
+      version: '*',
+      type: NodeDependencyType.Dev
+    }
+    addPackageJsonDependency(tree, uijarDependency)
+  }
+}
+
+function setupUIJarProject(): Rule {
+  return chain([addScriptsToPackageJson(), updateTsConfig(), replaceFiles()])
+}
+
+function addScriptsToPackageJson(): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    const json = JSON.parse(tree.read('./package.json')!.toString('utf-8'))
+
+    json.scripts['start:ui-jar'] =
+      'node node_modules/ui-jar/dist/bin/cli.js --directory ./src/app/ --includes \\.ts$ && ng serve ui-jar'
+
+    tree.overwrite('./package.json', JSON.stringify(json, null, 2))
+  }
+}
+
+function updateTsConfig(): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    const json = JSON.parse(tree.read('./projects/ui-jar/tsconfig.app.json')!.toString('utf-8'))
+
+    json.exclude
+      ? json.exclude.push('../../src/**/*.spec.ts')
+      : (json.exclude = ['../../src/**/*.spec.ts'])
+
+    json.include
+      ? json.include.push(['**/*.ts', '../../src/**/*.ts'])
+      : (json.include = ['**/*.ts', '../../src/**/*.ts'])
+
+    tree.overwrite('./projects/ui-jar/tsconfig.app.json', JSON.stringify(json, null, 2))
+  }
+}
+
+function replaceFiles(): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    const files = [
+      './projects/ui-jar/src/index.html',
+      './projects/ui-jar/src/main.ts',
+      './projects/ui-jar/src/styles.css'
+    ]
+
+    files.forEach(file => deleteFile(tree, file))
+
+    return mergeWith(apply(url('./files'), [move('./projects/ui-jar/src')]))
+  }
+}
+
+function deleteFile(tree: Tree, path: string): void {
+  if (tree.exists(path)) tree.delete(path)
+}
